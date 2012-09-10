11027c8b404625b5fd797a35621f74631ca7f9c4
Dmitriy Pakhtinov <ex.devote@gmail.com>
2012-09-10 09:33:22 +0400

    fix

--- core.class.js
+++ core.class.js
@@ -1,5 +1,5 @@
 /*
- * core.class.js Library for JavaScript v0.4.9.2
+ * core.class.js Library for JavaScript v0.4.9.3
  *
  * Copyright 2012, Dmitriy Pakhtinov ( spb.piksel@gmail.com )
  *
@@ -9,7 +9,7 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Update: 09-09-2012
+ * Update: 10-09-2012
  */
 
 (function( window, True, False, Null, undefined ) {
@@ -242,7 +242,7 @@
 										copy["__get"] ? "me.[__get].call(me,\"" + nm + "\")" : "" :
 										accessors[ prop ] && ( propType !== 5 || accessors[ prop ].get ) ?
 										"[(accessors)].[" + prop + "]" + ( propType === 5 ? ".get" : "" ) +
-										".call(me)" : "window.undefined" ) + ",[" + nm + "])",
+										".call(me,a)" : "window.undefined" ) + ",[" + nm + "])",
 										"End Property"
 									);
 								}
@@ -309,7 +309,14 @@
 
 						ownEach( copy, function( prop, val ) {
 							if ( !accessors.hasOwnProperty( prop ) ) {
-								owner.obj[ prop ] = val;
+								if ( ( !_extends || disableStatement ) && typeof val === "function" ) {
+									owner.obj[ prop ] = function() {
+										return val.apply( this === copy || this == window ? owner.obj : this, arguments );
+									}
+									owner.obj[ prop ].toString = function(){ return val.toString() }
+								} else {
+									owner.obj[ prop ] = val;
+								}
 							}
 						}, 1);
 
--- light.core.class.js
+++ light.core.class.js
@@ -1,5 +1,5 @@
 /*
- * light.core.class.js Library for JavaScript v0.4.9.2
+ * light.core.class.js Library for JavaScript v0.4.9.3
  *
  * Copyright 2012, Dmitriy Pakhtinov ( spb.piksel@gmail.com )
  *
@@ -9,7 +9,7 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Update: 09-09-2012
+ * Update: 10-09-2012
  */
 
 (function( window, True, False, Null, undefined ) {
@@ -309,7 +309,14 @@
 
 						ownEach( copy, function( prop, val ) {
 							if ( !accessors.hasOwnProperty( prop ) ) {
-								owner.obj[ prop ] = val;
+								if ( ( !_extends || disableStatement ) && typeof val === "function" ) {
+									owner.obj[ prop ] = function() {
+										return val.apply( this === copy || this == window ? owner.obj : this, arguments );
+									}
+									owner.obj[ prop ].toString = function(){ return val.toString() }
+								} else {
+									owner.obj[ prop ] = val;
+								}
 							}
 						}, 1);
 
